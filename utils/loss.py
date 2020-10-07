f7f9ce47d0d100cb469de54593da68d5503b0658
theroyakash <business.theroyakash@outlook.com>
2020-10-07 14:04:06 +0530

    Create loss.py

---
+++ utils/loss.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Loss class
+class Loss(object):
+    def loss(self, y_true, y_pred):
+        return NotImplementedError()
+
+    def gradient(self, y, y_pred):
+        raise NotImplementedError()
+
+    def acc(self, y, y_pred):
+        return 0
+
+# Squard Error Loss
+class SquareLoss(Loss):
+    def __init__(self): 
+        pass
+
+    def loss(self, y, y_pred):
+        return 0.5 * np.power((y - y_pred), 2)
+
+    def gradient(self, y, y_pred):
+        return -(y - y_pred)

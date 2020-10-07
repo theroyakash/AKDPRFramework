8ce3f533345798933dd98531c97f2ceb38d17313
theroyakash <business.theroyakash@outlook.com>
2020-10-07 14:08:14 +0530

    RMSProp and CE Loss added
    RMSProp and CE Loss added

--- utils/loss.py
+++ utils/loss.py
@@ -21,3 +21,27 @@ class SquareLoss(Loss):
 
     def gradient(self, y, y_pred):
         return -(y - y_pred)
+
+# Cross Entropy Loss
+class CrossEntropy(Loss):
+    def __init__(self): pass
+
+    def loss(self, y, p):
+        # Avoid division by zero
+        p = np.clip(p, 1e-15, 1 - 1e-15)
+        return - y * np.log(p) - (1 - y) * np.log(1 - p)
+    
+    def accuracy_score(self, y_true, y_pred):
+        """
+        Compare y_true to y_pred and return the accuracy
+        """
+        accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
+        return accuracy
+
+    def acc(self, y, p):
+        return self.accuracy_score(np.argmax(y, axis=1), np.argmax(p, axis=1))
+
+    def gradient(self, y, p):
+        # Avoid division by zero
+        p = np.clip(p, 1e-15, 1 - 1e-15)
+        return - (y / p) + (1 - y) / (1 - p)
--- utils/optimizers.py
+++ utils/optimizers.py
@@ -31,6 +31,25 @@ class Adam():
         m_hat = self.m / (1 - self.b1)
         v_hat = self.v / (1 - self.b2)
 
-        self.w_updt = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
+        self.w_updt = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
 
         return w - self.w_updt
+
+
+class RMSprop():
+    def __init__(self, learning_rate=0.01, rho=0.9):
+        self.learning_rate = learning_rate
+        self.Eg = None # Running average of the square gradients at w
+        self.epsilon = 1e-8
+        self.rho = rho
+
+    def update(self, w, grad_wrt_w):
+        # If not initialized
+        if self.Eg is None:
+            self.Eg = np.zeros(np.shape(grad_wrt_w))
+
+        self.Eg = self.rho * self.Eg + (1 - self.rho) * np.power(grad_wrt_w, 2)
+
+        # Divide the learning rate for a weight by a running average of the magnitudes of recent
+        # gradients for that weight
+        return w - self.learning_rate *  grad_wrt_w / np.sqrt(self.Eg + self.epsilon)

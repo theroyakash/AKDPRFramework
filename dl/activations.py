6b708429d1c9ebff4c94d9486509b95584366141
theroyakash <business.theroyakash@outlook.com>
2020-10-09 10:22:21 +0530

    activations added

---
+++ dl/activations.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+# Sigmoid activations
+class Sigmoid():
+    def __call__(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def gradient(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
+
+# Softmax activations
+class Softmax():
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def gradient(self, x):
+        p = self.__call__(x)
+        return p * (1 - p)
+
+# tanh activations
+class TanH():
+    def __call__(self, x):
+        return 2 / (1 + np.exp(-2*x)) - 1
+
+    def gradient(self, x):
+        return 1 - np.power(self.__call__(x), 2)
+
+# ReLU
+class ReLU():
+    def __call__(self, x):
+        return np.where(x >= 0, x, 0)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, 0)
+
+# LeakyReLU
+class LeakyReLU():
+    def __init__(self, alpha=0.2):
+        self.alpha = alpha
+
+    def __call__(self, x):
+        return np.where(x >= 0, x, self.alpha * x)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, self.alpha)
+
+# SoftPlus
+class SoftPlus():
+    def __call__(self, x):
+        return np.log(1 + np.exp(x))
+
+    def gradient(self, x):
+        return 1 / (1 + np.exp(-x))

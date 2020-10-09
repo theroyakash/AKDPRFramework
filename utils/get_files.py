54287ab7b5fe3ffb4269bbacd42c121df37d7bf1
theroyakash <business.theroyakash@outlook.com>
2020-10-09 10:16:05 +0530

    Update get_files.py

--- utils/get_files.py
+++ utils/get_files.py
@@ -1,10 +1,18 @@
 # Downloads files from the web
 import requests
 from PIL import Image
-
-def download(url):
-    result = requests.get(url, allow_redirects=True)
-    return result
+import io
+import os
+import sys
 
 def downloadImageFromURL(url):
-    pass
+    '''
+    Downloads Image from any image URL
+    To know more about what are image URL visit here: https://bit.ly/what-are-imageurl
+    '''
+    b = requests.get(url).content
+    image = Image.open(io.BytesIO(b))
+    image.save("saved.jpg")
+    print(f'Image saved at {os.getcwd()}/saved.jpg')
+
+downloadImageFromURL('https://images.unsplash.com/photo-1561731216-c3a4d99437d5')

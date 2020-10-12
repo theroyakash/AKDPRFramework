e1d964944512c96e49dc1259d63d632151225331
theroyakash <business.theroyakash@outlook.com>
2020-10-12 23:45:45 +0530

    Update Regression.py

--- AKDPRFramework/mlops/Regression.py
+++ AKDPRFramework/mlops/Regression.py
@@ -1,1 +1,103 @@
+from tqdm import tqdm
 import numpy as np
+import math
+
+class L1():
+    def __init__(self, alpha):
+        self.alpha = alpha
+    
+    def __call__(self, w):
+        return self.alpha * np.linalg.norm(w)
+
+    def grad(self, w):
+        return self.alpha * np.sign(w)
+
+class L2():
+    def __init__(self, alpha):
+        self.alpha = alpha
+    
+    def __call__(self, w):
+        return self.alpha * 0.5 *  w.T.dot(w)
+
+    def grad(self, w):
+        return self.alpha * w
+
+class L1L2():
+    def __init__(self, alpha, l1_ratio=0.5):
+        self.alpha = alpha
+        self.l1_ratio = l1_ratio
+
+    def __call__(self, w):
+        l1_contr = self.l1_ratio * np.linalg.norm(w)
+        l2_contr = (1 - self.l1_ratio) * 0.5 * w.T.dot(w) 
+        return self.alpha * (l1_contr + l2_contr)
+
+    def grad(self, w):
+        l1_contr = self.l1_ratio * np.sign(w)
+        l2_contr = (1 - self.l1_ratio) * w
+        return self.alpha * (l1_contr + l2_contr)
+
+class Regression(object):
+    def __init__(self, iterations, learning_rate, regularization):
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+        self.regularization = regularization()
+    
+    def random_initialization(self, number_of_features):
+        '''
+        Random initialization of weights.
+        '''
+        lim = 1 / math.sqrt(number_of_features)
+        self.w = np.random.uniform(-lim, lim, (number_of_features, ))
+
+    def fit(self,X, y):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        self.training_errors = []
+        self.random_initialization(number_of_features=X.shape[1])
+
+        for _ in tqdm(range(self.iterations)):
+            y_pred = X.dot(self.w)
+            # Calculate l2 loss
+            mse = np.mean(0.5 * (y - y_pred)**2 + self.regularization(self.w))
+            self.training_errors.append(mse)
+            # Gradient of l2 loss w.r.t w
+            grad_w = -(y - y_pred).dot(X) + self.regularization.grad(self.w)
+            # Update the weights
+            self.w -= self.learning_rate * grad_w
+
+    def predict(self, X):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = X.dot(self.w)
+        return y_pred
+
+
+class LinearRegression(Regression):
+    """
+    Linear Regression model
+    
+        Args:
+            - ``iterations``: The number of training iterations the algorithm will tune the weights for.
+            - ``learning_rate``: The step length that will be used when updating the weights.
+            - ``gradient_descent``: True or False depending if gradient descent should be used when training. If False then we use batch optimization by least squares.
+    """
+    def __init__(self, iterations=1000, learning_rate=1e-3, gradient_descent=True):
+        self.gradient_descent = gradient_descent
+        # No regularization
+        self.regularization = lambda x: 0
+        self.regularization.grad = lambda x: 0
+        super(LinearRegression, self).__init__(iterations=iterations,
+                                            learning_rate=learning_rate, regularization=self.regularization)
+    def fit(self, X, y):
+        # If not gradient descent => Least squares approximation of w
+        if not self.gradient_descent:
+            # Insert constant ones for bias weights
+            X = np.insert(X, 0, 1, axis=1)
+            # Calculate weights by least squares (using Moore-Penrose pseudoinverse)
+            U, S, V = np.linalg.svd(X.T.dot(X))
+            S = np.diag(S)
+            X_sq_reg_inv = V.dot(np.linalg.pinv(S)).dot(U.T)
+            self.w = X_sq_reg_inv.dot(X.T).dot(y)
+        else:
+            super(LinearRegression, self).fit(X, y)

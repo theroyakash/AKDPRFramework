e691f380026480e3a0911f1df7196a46090f8257
theroyakash <business.theroyakash@outlook.com>
2020-10-13 14:54:22 +0530

    imported files

--- AKDPRFramework/__init__.py
+++ AKDPRFramework/__init__.py
@@ -1,2 +1,5 @@
 # Init
 __version__ = "release-candidate-1.0.0--alpha"
+from .utils import dataops, get_files
+from .loss import loss
+from .dl import layers, activations, neuralnets
--- AKDPRFramework/dl/__init__.py
+++ AKDPRFramework/dl/__init__.py
@@ -1,3 +1,3 @@
 from .activations import Sigmoid, Softmax, TanH, ReLU, LeakyReLU, SoftPlus
 from .layers import Dense, Conv2D
-from .nn import NeuralNetworks
+from .neuralnets import NeuralNetworks

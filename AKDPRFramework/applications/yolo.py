2664334a3e7121c81150c1368649a281fbd59102
theroyakash <business.theroyakash@outlook.com>
2020-10-18 15:19:41 +0530

    YOLO model complete, loss implementation started

--- AKDPRFramework/applications/yolo.py
+++ AKDPRFramework/applications/yolo.py
@@ -1,3 +1,11 @@
+"""
+PyTorch Implementation of Deep Learning Framework YOLO (Original paper)
+With slight modifications of added Dropout and BatchNormalization
+
+This module contains the Model Architecture for YOLO Model is a part of AKDPRFramework
+Author: ©2020 theroyakash
+"""
+
 import torch
 import torch.nn as nn
 
@@ -41,3 +49,74 @@ class YOLOOriginal(nn.Module):
     def __init__(self, in_channels=3, **kwargs):
         super(YOLOOriginal, self).__init__()
         self.in_channels = in_channels
+        self.architecture = architecture_config
+        self.darknet = self._create_conv_layers(self.architecture)
+        self.fcs = self._create_fcs(**kwargs)
+
+    def forward(self, x):
+        x = self.darknet(x)
+        return self.fcs(torch.flatten(x, start_dim=1))
+
+    def _create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for x in architecture:
+            if type(x) == tuple:
+                layers += [
+                    CNNBlock(
+                        in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3]
+                    )
+                ]
+                in_channels = x[1]
+
+            elif type(x) == str:
+                layers += [nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))]
+
+            elif type(x) == list:
+                conv1 = x[0]
+                conv2 = x[1]
+                number_of_repeats = x[2]
+
+                for _ in range(number_of_repeats):
+                    layers += [
+                        CNNBlock(
+                            in_channels,
+                            conv1[1],
+                            kernel_size=conv1[0],
+                            stride=conv1[2],
+                            padding=conv1[3],
+                        )
+                    ]
+                    layers += [
+                        CNNBlock(
+                            conv1[1],
+                            conv2[1],
+                            kernel_size=conv2[0],
+                            stride=conv2[2],
+                            padding=conv2[3],
+                        )
+                    ]
+                    in_channels = conv2[1]
+
+        return nn.Sequential(*layers)
+
+    def _create_fcs(self, split_size, nb_boxes, nb_classes):
+        """
+        Creates the fully connected layer at the end.
+            Args:
+                - split_size: (int) Mention the split size
+                - nb_boxes: Number of BOXES
+                - nb_classes: number of classes
+
+            Returns:
+                Sequential pytorch model.
+        """
+
+        return nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * split_size * split_size, 496),
+            nn.Dropout(0.2),
+            nn.LeakyReLU(0.1),
+            nn.Linear(496, split_size * split_size * (nb_classes + nb_boxes * 5))
+        )
---
+++ AKDPRFramework/applications/yololoss.py
@@ -0,0 +1,27 @@
+"""
+Implementation of YOLO Loss algorithm from original YOLO paper
+
+Usage: from AKDPRFramework.applications.yololoss import YoloLoss
+"""
+
+import torch
+import torch.nn as nn
+
+class YoloLoss(nn.Module):
+    def __init__(self, split_size, nb_boxes, nb_classes):
+        """
+        YoloLoss intializer
+            Args:
+                split_size: split_size for the image. In original paper split_size was 7
+                nb_boxes: number of boxes original paper has implemented 2
+                nb_classes: number of classes. Pascal VOC dataset which was used by the paper was 20
+        """
+        self.split_size = split_size
+        self.nb_boxes = nb_boxes
+        self.nb_classes = nb_classes
+
+        # Mentioned in the paper the following variables indicates how much we should pay loss for no object and bx coordinates
+        self.lambda_no_object = 0.5
+        self.lambda_no_coordinates = 5
+
+        
